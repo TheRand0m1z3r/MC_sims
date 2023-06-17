13231ca354cd37c1c91c00f3ff8c16ba8321ddd3
Eytan Balken <eytan.balken@mail.huji.ac.il>
2023-06-17 20:55:14 +0300

    Added ex 2 and  3
    Signed-off-by: Eytan Balken <eytan.balken@mail.huji.ac.il>

--- Cellular_Automata.py
+++ Cellular_Automata.py
@@ -1,50 +1,48 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.integrate import simpson
 
-rng = np.random.default_rng(1234)
+rng = np.random.default_rng()
 
-def build_road(iters, L, N_cars):
-    road = np.zeros([iters, L])
-    road[0, :N_cars] = 1
-    rng.shuffle(road[0, :])
-    # print(road[0, :])
+def NS_Algo(L, N_cars, v_max, iterations, rand_slow=0):
+    def build_road(iters, L, N_cars):
+        road = np.zeros([iters, L])
+        road[0, :N_cars] = 1
+        rng.shuffle(road[0, :])
 
-    return road
+        return road
 
-def find_empty_cells_in_front_of_car(d, lane, L):
-    for i in range(L):
-        if lane[i] == 0:
-            continue
-        else:
-            # print(i, (i+1) % L, (i+2) % L)
-            if (lane[int((i+1) % L)] == 0) & (lane[int((i+2) % L)] == 0):
-                d[i] = 2
-            elif lane[int((i+1) % L)] == 0:
-                d[i] = 1
+    def find_empty_cells_in_front_of_car(d, lane, L):
+        for i in range(L):
+            if lane[i] == 0:
+                continue
             else:
-                d[i] = 0
-    return d
+                if (lane[int((i + 1) % L)] == 0) & (lane[int((i + 2) % L)] == 0):
+                    d[i] = 2
+                elif lane[int((i + 1) % L)] == 0:
+                    d[i] = 1
+                else:
+                    d[i] = 0
+        return d
 
-def move_cars(lane, v):
-    new_lane = np.zeros([len(lane)])
-    new_v = np.zeros([len(lane)])
-    for i in range(len(lane)):
-        if lane[i] == 0:
-            continue
-        else:
-            # lane[i] = 0
-            # print((i+v[i]) % len(lane))
-            new_lane[int((i+v[i]) % len(lane))] = 1
-            new_v[int((i+v[i]) % len(lane))] = v[i]
-            # v[i] = 0
-    # return lane, v
-    return new_lane, new_v
+    def move_cars(lane, v):
+        new_lane = np.zeros([len(lane)])
+        new_v = np.zeros([len(lane)])
+        for i in range(len(lane)):
+            if lane[i] == 0:
+                continue
+            else:
+                new_lane[int((i + v[i]) % len(lane))] = 1
+                new_v[int((i + v[i]) % len(lane))] = v[i]
+        return new_lane, new_v
 
-def NS_Algo(L, N_cars, v_max, iterations,rand_slow):
     road = build_road(iterations, L, N_cars)
+    # print(road)
+    flux = np.zeros([iterations])
     d = np.copy(road)
     d[0, :] = find_empty_cells_in_front_of_car(d[0, :], road[0, :], L)
     v = np.copy(d)
+    flux[0] = (np.sum(v[0, :] == 1) + 2 * np.sum(v[0, :] == 2)) / L
 
     for t in range(1, iterations):
         ## Acceleration
@@ -55,25 +53,124 @@ def NS_Algo(L, N_cars, v_max, iterations,rand_slow):
         randomize = rng.random([L])
         v[t, randomize < rand_slow] = np.maximum(v[t, randomize < rand_slow] - 1, 0)
         ## Car motion
-        # print(road[t-1, :])
         road[t, :], v[t, :] = move_cars(road[t-1, :], v[t, :])
         d[t, :] = find_empty_cells_in_front_of_car(d[t, :], road[t, :], L)
+        ## Finding flux
+        flux[t] = (np.sum(v[t, :] == 1) + 2 * np.sum(v[t, :] == 2)) / L
 
-    return road
+    return road, flux
+
+def power_distribution(y_min, alpha, N=1):
+    ## This returns a power distribution with a minimum value of y_min and a power of alpha
+
+    k = alpha * y_min**alpha
+    x = rng.random(N)
+    y = (1 / (y_min**(-alpha) - (alpha/k) * x))**(1/alpha)
+
+    return y
+
+def test_power_distribution(y_min, alpha, N=10000):
+    ## This tests the power distribution function by plotting a histogram of the results
+
+    k = alpha * y_min**alpha
+
+    y = power_distribution(y_min, alpha, N)
+    x = np.linspace(y_min, np.max(y), N)
+    y_theory = k * x ** (-(alpha + 1))
+    print(simpson(y_theory, x))
+
+    plt.figure()
+    plt.hist(y, bins=100, density=True, label="Simulation")
+    plt.plot(x, y_theory, 'r')
+    plt.title("Power Distribution Histogram")
+    plt.ylabel("Count")
+    plt.xlabel("y")
+    # plt.savefig("Power_Distribution_Histogram.png")
+    plt.show()
+    # plt.close()
+
+def simulate_RW(L, iterations):
+    # moves = rng.choice([-1, 1], size=iterations)
+    t = np.zeros(iterations)
+
+    for i in range(iterations):
+        print("Iteration: " + str(i+1))
+        lattice = np.zeros(L)
+        lattice[0] = 1
+        is_in_sys = True
+
+        while is_in_sys:
+            if lattice[0]:
+                lattice = np.roll(lattice, 1)
+                t[i] += 1
+            else:
+                move = rng.choice([-1, 1])
+                if not ((lattice[-1]) and (move == 1)):
+                    lattice = np.roll(lattice, move)
+                    t[i] += 1
+                else:
+                    is_in_sys = False
+                    t[i] += 1
+
+    return t
 
 if __name__ == '__main__':
     print("Lets go!")
-    L = 1000
-    N_cars = [100]#, 200, 333, 500]
-    v_max = 2
-    iterations = 1000
-    rand_slow = 0.01
-    for N in N_cars:
-        road = NS_Algo(L, N, v_max, iterations,rand_slow)
-        plt.figure()
-        plt.pcolormesh(road)
-        plt.title("N = " + str(N) + " cars")
-        plt.ylabel("Time")
-        plt.xlabel("Road")
-        plt.savefig(str(N) + "_cars.png")
-        plt.close()
+    # ## Exercise 1:
+    # L_lane = 1000
+    # N_cars = [100, 200, 333, 500]
+    # v_max = 2
+    # iter_traffic = 2500
+    # rand_slow = 0.05
+    # for N in N_cars:
+    #     road, flux = NS_Algo(L_lane, N, v_max, iter_traffic)
+    #
+    #     plt.figure()
+    #     plt.pcolormesh(road)
+    #     plt.title("N = " + str(N) + " cars")
+    #     plt.ylabel("Time")
+    #     plt.xlabel("Road")
+    #     plt.savefig(str(N) + "_cars_no_slowing.png")
+    #     plt.close()
+    #
+    #     plt.figure()
+    #     plt.plot(flux)
+    #     plt.title("flux vs time, N = " + str(N) + " cars, no slowing")
+    #     plt.ylabel("flux")
+    #     plt.xlabel("time")
+    #     plt.savefig(str(N) + "_cars_flux_no_slowing.png")
+    #     plt.close()
+    #
+    #     road, flux = NS_Algo(L_lane, N, v_max, iter_traffic, rand_slow)
+    #
+    #     plt.figure()
+    #     plt.pcolormesh(road)
+    #     plt.title("N = " + str(N) + " cars")
+    #     plt.ylabel("Time")
+    #     plt.xlabel("Road")
+    #     plt.savefig(str(N) + "_cars_rand_" + str(rand_slow) + ".png")
+    #     plt.close()
+    #
+    #     plt.figure()
+    #     plt.plot(flux)
+    #     plt.title("flux vs time, N = " + str(N) + " cars, rand_slow = " + str(rand_slow))
+    #     plt.ylabel("flux")
+    #     plt.xlabel("time")
+    #     plt.savefig(str(N) + "_cars_flux_rand_" + str(rand_slow) + ".png")
+    #     plt.close()
+    #
+    # print("Done with traffic jams!")
+    #
+    # ## Exercise 2:
+    # y_min = 0.1
+    # alpha = 1.5
+    # N = 5000
+    # y = test_power_distribution(y_min, alpha, N)
+    #
+    # print("Done with power distribution!")
+
+    ## Exercise 3:
+    L_RW = 50
+    iter_RW = 1000
+    time = simulate_RW(L_RW, iter_RW)
+    mean_time = np.mean(time)
